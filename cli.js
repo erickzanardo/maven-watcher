f2dd4dcd6009c4477d225e25249b19ebadf77d44
Erick Zanardo <erickzanardoo@gmail.com>
2015-05-14 20:57:05 -0300

    Parsing pom

--- cli.js
+++ cli.js
@@ -1,6 +1,22 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
+var path = require('path');
+
 var cwd = process.cwd();
 var Watcher = require('./index.js');
 
-new Watcher(cwd).watch();
+var pomPath = path.join(cwd, 'pom.xml').toString();
+fs.open(pomPath, "r+", function(error) {
+  if(!error) {
+    new Watcher(cwd).watch();
+  } else {
+    switch(error.code) {
+      case "EACCES":
+        console.error("Can't open", pomPath);
+      break;
+    default:
+      console.error('This is not a Maven project')
+    }
+  }
+});
--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-var findit = require('findit');
+var findit = require('findit')
+  , cheerio = require('cheerio')
+  , fs = require('fs')
+  , path = require('path');
 
 function Watcher(path) {
   this.path = path;
 }
 
-Watcher.prototype.watch = function() {
+Watcher.prototype.parseTargetFolder = function(done) {
+  var pomPath = path.join(this.path, 'pom.xml');
+  fs.readFile(pomPath, function(err, data) {
+    $ = cheerio.load(data);
+    var artifactId = $('project > artifactId').text();
+    var version = $('project > version').text();
+    var targetFolder = [artifactId, version].join('-');
+    this.targetPath = path.join(this.path, 'target', targetFolder);
+    done.apply(this);
+  }.bind(this));
+};
+
+Watcher.prototype.readRootFolder = function() {
   var finder = findit(this.path);
+
+  // TODO find src/main/resources
+  // TODO find src/main/webapp
   
   finder.on('directory', function(dir, stat, stop) {
     console.log(dir);
@@ -14,6 +32,10 @@ Watcher.prototype.watch = function() {
   finder.on('file', function(file, stat) {
     console.log(file);
   });
+};
+
+Watcher.prototype.watch = function() {
+  this.parseTargetFolder(this.readRootFolder);
 }
 
 module.exports = Watcher;
